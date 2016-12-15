6d130025eb2997f2cb4729dc2a109410d8568bfa
Dheeraj Kumar G <guntidheerajkumar@gmail.com>
2016-12-15 17:00:29 +0530

    Twitter Style New Data Alert
    Twitter Style New Data Alert

---
+++ NewDataAlert/NewAlert.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using UIKit;
+using CoreGraphics;
+using Foundation;
+
+namespace NewDataAlert
+{
+	public class NewAlert
+	{
+		private bool isUpdatedAddedInWindow;
+		private UIButton updateBtn;
+		private bool isUpdateVisible;
+		private bool hideOnTouch;
+		private double showAnimationInterval;
+		private double hideAnimationInterval;
+		private float heightOffset;
+		private UIColor backgroundColor;
+		private UIColor textColor;
+		private UIFont textFont;
+		private float height;
+		private float borderWidth;
+		private UIColor borderColor;
+		public event EventHandler Process;
+
+		public NewAlert()
+		{
+			isUpdatedAddedInWindow = false;
+			textFont = UIFont.SystemFontOfSize(12.5f);
+			height = 30f;
+			isUpdateVisible = false;
+			hideOnTouch = true;
+			showAnimationInterval = 1.0;
+			hideAnimationInterval = 0.7;
+			heightOffset = 78.0f;
+			backgroundColor = UIColor.FromRGBA(0f, 153 / 255f, 229 / 255f, 1f);
+			textColor = UIColor.White;
+			borderWidth = 0.0f;
+			borderColor = UIColor.DarkGray;
+		}
+
+		public void ShowMessage(string message, UIView view)
+		{
+			if (!isUpdatedAddedInWindow) {
+
+				nfloat width = message.Length;
+				if (width <= view.Frame.Width) {
+					width = view.Frame.Width - 10.0f;
+				}
+				updateBtn = new UIButton(new CGRect(0, -50, 100, height));
+				updateBtn.SetTitle(message, UIControlState.Normal);
+				var frame = updateBtn.Frame;
+				frame.X = (view.Frame.Width / 2f) - 50;
+				updateBtn.Frame = frame;
+				updateBtn.BackgroundColor = backgroundColor;
+				updateBtn.Layer.CornerRadius = height / 2.0f;
+				updateBtn.Layer.MasksToBounds = true;
+				updateBtn.TitleLabel.TextAlignment = UITextAlignment.Center;
+				updateBtn.SetTitleColor(textColor, UIControlState.Normal);
+				updateBtn.TitleLabel.Font = textFont;
+				updateBtn.Layer.BorderWidth = borderWidth;
+				updateBtn.Layer.BorderColor = borderColor?.CGColor;
+
+				updateBtn.AddTarget(UpdateButtonTouchUpInside, UIControlEvent.TouchUpInside);
+				view.AddSubview(updateBtn);
+				isUpdatedAddedInWindow = true;
+			}
+
+			if (!isUpdateVisible) {
+				isUpdateVisible = true;
+				UIView.AnimateNotify(showAnimationInterval, 0f, 0.5f, 0.5f, UIViewAnimationOptions.CurveEaseIn, () => {
+					var frame = updateBtn.Frame;
+					frame.Y = this.heightOffset;
+					updateBtn.Frame = frame;
+				}, (finished) => { });
+			}
+		}
+
+		void UpdateButtonTouchUpInside(object sender, EventArgs e)
+		{
+			UIView.Animate(0.1f, () => {
+				this.updateBtn.Transform = CGAffineTransform.MakeScale(1.0f, 1.0f);
+			});
+
+			if (this.hideOnTouch) {
+				if (Process != null) {
+					Process(this, new EventArgs());
+				}
+				HideUpdate();
+			}
+		}
+
+		void HideUpdate()
+		{
+			isUpdateVisible = false;
+			isUpdatedAddedInWindow = false;
+			UIView.AnimateNotify(hideAnimationInterval, 0.4f, 0.5f, 0.8f, UIViewAnimationOptions.CurveEaseInOut, () => {
+				var frame = this.updateBtn.Frame;
+				frame.Y = -50;
+				this.updateBtn.Frame = frame;
+			}, (finished) => {
+				updateBtn.RemoveFromSuperview();
+			});
+		}
+	}
+}
---
+++ SampleApp/ViewController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CoreGraphics;
+using CoreAnimation;
+using Foundation;
+using UIKit;
+using NewDataAlert;
+
+namespace SampleApp
+{
+	public partial class ViewController : UIViewController
+	{
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			NewAlert alert = new NewAlert();
+			alert.ShowMessage("New Tweets", this.View);
+			alert.Process += (sender, e) => {
+				this.View.BackgroundColor = UIColor.Yellow;
+			};
+		}
+	}
+}
